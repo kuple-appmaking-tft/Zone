d421e87003a423f5baf160ff4439edf1796e5384
sejin <sejin2740@korea.ac.kr>
2020-05-30 01:13:36 +0900

    Merge branch 'master' of C:\Users\Sejin\AndroidStudioProjects\zone with conflicts.

--- app/src/main/java/com/kuple/zone/commonboard/CommonboardActivity.java
+++ app/src/main/java/com/kuple/zone/commonboard/CommonboardActivity.java
@@ -14,4 +14,4 @@ public class CommonboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_commonboard);
     }
 }
-//////////
+//////////////////////////////////////////////////
