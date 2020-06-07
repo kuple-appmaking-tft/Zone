940434e02cc30e48208af378eca45f8e83eb2e9e
beongyoung <sergiopark10373@likelion.org>
2020-06-08 00:29:09 +0900

    1. 프래그먼트 전환 함수 변경

--- app/src/main/java/com/kuple/zone/navigation/UserFragment.java
+++ app/src/main/java/com/kuple/zone/navigation/UserFragment.java
@@ -1,15 +1,106 @@
 package com.kuple.zone.navigation;
 
-import androidx.fragment.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import com.kuple.zone.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class UserFragment extends Fragment{
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        return view;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kuple.zone.R;
+import com.kuple.zone.login.LoginActivity;
+
+public class UserFragment extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "UserFragment";
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+    private TextView textivewDelete;
+
+    private TextView textviewNickname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_user);
+
+        initializeViewGroup();
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+        //유저가 로그인 하지 않은 상태라면 null 상태이고 이 액티비티를 종료하고 로그인 액티비티를 연다.
+        if(firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //유저가 있다면, null이 아니면 계속 진행
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //textViewUserEmail의 내용을 변경해 준다.
+        textViewUserEmail.setText("반갑습니다.\n"+ user.getEmail()+"으로 로그인 하였습니다.");
+        textviewNickname.setText("이메일 : "+ user.getDisplayName());
+
+        //logout button event
+        buttonLogout.setOnClickListener(this);
+        textivewDelete.setOnClickListener(this);
+
+    }
+
+    public void initializeViewGroup(){
+        //initializing views
+        textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        textivewDelete = (TextView) findViewById(R.id.textviewDelete);
+        textviewNickname = (TextView) findViewById(R.id.textviewNickname);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        //회원탈퇴를 클릭하면 회원정보를 삭제한다. 삭제전에 컨펌창을 하나 띄워야 겠다.
+        if(view == textivewDelete) {
+            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(UserFragment.this);
+            alert_confirm.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(UserFragment.this, "계정이 삭제 되었습니다.", Toast.LENGTH_LONG).show();
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                        }
+                                    });
+                        }
+                    }
+            );
+            alert_confirm.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(UserFragment.this, "취소", Toast.LENGTH_LONG).show();
+                }
+            });
+            alert_confirm.show();
+        }
     }
 }
