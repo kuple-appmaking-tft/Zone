db5b716d1d4ea3dbc0337b5b01bb42e7d0964ece
beongyoung <50867167+beongyoung@users.noreply.github.com>
2020-07-01 19:26:47 +0900

    commit : 모델 생성

--- app/src/main/java/com/kuple/zone/model/Timetable.java
+++ app/src/main/java/com/kuple/zone/model/Timetable.java
@@ -5,4 +5,24 @@ public class Timetable {
     private int code;
     private String subject;
 
+    public int getCode() {
+        return code;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Timetable{" +
+                "time=" + time +
+                ", code=" + code +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
 }
