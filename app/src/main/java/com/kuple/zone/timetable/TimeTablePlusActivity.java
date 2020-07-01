dc61a97d09154ade6f2a64dab0202bda2515dba7
sejin <sejin2740@korea.ac.kr>
2020-07-02 03:36:39 +0900

    시간표 및 시간표 추가 디자인

---
+++ app/src/main/java/com/kuple/zone/timetable/TimeTablePlusActivity.java
@@ -0,0 +1,19 @@
+package com.kuple.zone.timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.kuple.zone.R;
+
+public class TimeTablePlusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_table_plus);
+
+
+    }
+}
