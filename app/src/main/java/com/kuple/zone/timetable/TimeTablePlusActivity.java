e627da1fd5e5a5467421b79967e8f806781a7570
sejin <sejin2740@korea.ac.kr>
2020-07-03 15:55:09 +0900

    시간표 및 시간표 추가 디자인

--- app/src/main/java/com/kuple/zone/timetable/TimeTablePlusActivity.java
+++ app/src/main/java/com/kuple/zone/timetable/TimeTablePlusActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.kuple.zone.R;
 
 public class TimeTablePlusActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table_plus);
-       
 
 
     }
