799aa95395b5ab97fd445116dad3c45de0604986
beongyoung <50867167+beongyoung@users.noreply.github.com>
2020-08-18 12:35:34 +0900

    #19 3.피드 모델 업데이트

--- app/src/main/java/com/kuple/zone/Adapter/FeedAdapter.java
+++ app/src/main/java/com/kuple/zone/Adapter/FeedAdapter.java
@@ -1,7 +1,86 @@
 package com.kuple.zone.Adapter;
 
-import com.kuple.zone.model.BoardInfo;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.kuple.zone.R;
+import com.kuple.zone.model.FeedModel;
+
+import java.util.ArrayList;
 
 
-public class FeedAdapter {
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder>{
+
+    Context context;
+    ArrayList<FeedModel> FeedArrayList = new ArrayList<>();
+    RequestManager glide;
+
+    public FeedAdapter(Context context, ArrayList<FeedModel> modelFeedArrayList) {
+
+        this.context = context;
+        this.FeedArrayList = modelFeedArrayList;
+        glide = Glide.with(context);
+
+    }
+
+    @Override
+    public FeedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_feed, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final FeedModel modelFeed = FeedArrayList.get(position);
+
+        holder.tv_name.setName(modelFeed.getName());
+        holder.tv_time.setText(modelFeed.getTime());
+        holder.tv_likes.setText(String.valueOf(modelFeed.getLikes()));
+        holder.tv_comments.setText(modelFeed.getComments() + " comments");
+        holder.tv_post.setText(modelFeed.getStatus());
+
+        glide.load(modelFeed.getPropic()).into(holder.img_profile);
+
+        if (modelFeed.getPostpic() == 0) {
+            holder.img_post.setVisibility(View.GONE);
+        } else {
+            holder.img_post.setVisibility(View.VISIBLE);
+            glide.load(modelFeed.getPostpic()).into(holder.img_post);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return FeedArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_name, tv_time, tv_likes, tv_comments, tv_post;
+        ImageView img_profile, img_post;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            img_profile = (ImageView) itemView.findViewById(R.id.img_profile);
+            img_post = (ImageView) itemView.findViewById(R.id.img_post);
+
+            tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            tv_time = (TextView) itemView.findViewById(R.id.tv_time);
+            tv_likes = (TextView) itemView.findViewById(R.id.tv_like);
+            tv_comments = (TextView) itemView.findViewById(R.id.tv_comment);
+            tv_post = (TextView) itemView.findViewById(R.id.tv_post);
+        }
+    }
 }
--- app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
+++ app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
@@ -6,10 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.kuple.zone.Adapter.FeedAdapter;
 import com.kuple.zone.R;
+import com.kuple.zone.model.FeedModel;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
+    RecyclerView recyclerView;
+    ArrayList<FeedModel> modelFeedArrayList = new ArrayList<>();
+    FeedAdapter adapterFeed;
+
+    @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         return view;
@@ -22,5 +34,36 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             case R.id.btnMeal:
             case R.id.btnTimeTable:
         }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+
+        recyclerView = (RecyclerView) recyclerView.findViewById(R.id.feedKuple);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapterFeed = new FeedAdapter(this, modelFeedArrayList);
+        recyclerView.setAdapter(adapterFeed);
+
+        populateRecyclerView();
+    }
+
+    public void populateRecyclerView() {
+
+        FeedModel modelFeed = new FeedModel(1, 9, 2, R.drawable.ic_profile_24dp, R.drawable.ic_profile_24dp,
+                "Sajin Maharjan", "2 hrs", "The cars we drive say a lot about us.");
+        modelFeedArrayList.add(modelFeed);
+        modelFeed = new FeedModel(2, 26, 6, R.drawable.rounded_profile, 0,
+                "Karun Shrestha", "9 hrs", "Don't be afraid of your fears. They're not there to scare you. They're there to \n" +
+                "let you know that something is worth it.");
+        modelFeedArrayList.add(modelFeed);
+        modelFeed = new FeedModel(3, 17, 5, R.drawable.ic_home, R.drawable.img_link,
+                "Lakshya Ram", "13 hrs", "That reflection!!!");
+        modelFeedArrayList.add(modelFeed);
+
+        adapterFeed.notifyDataSetChanged();
     }
 }
