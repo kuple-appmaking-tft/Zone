57c4949d6c141372090bd34438d0c8893f7aca54
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-09-19 20:21:31 +0900

    g

--- app/src/main/java/com/kuple/zone/Adapter/FeedAdapter.java
+++ app/src/main/java/com/kuple/zone/Adapter/FeedAdapter.java
@@ -29,6 +29,7 @@ import com.kuple.zone.model.BoardInfo;
 import com.kuple.zone.model.SliderItem;
 import com.kuple.zone.model.UserModel;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -36,7 +37,7 @@ import javax.annotation.Nonnull;
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder>{
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
-    private List<BoardInfo> list_feed;
+    private ArrayList<BoardInfo> arrayList_feed;
     private Context mContext;
     private FirebaseUser mFirebaseUser;//현재 사용중인 앱의 주인의 정보 .getCurrent 까지 된정보
     private OnItemClick mCallback;
@@ -44,13 +45,12 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder
     private String mBoardName;
     private RequestManager glide;
 
-    public FeedAdapter(List<BoardInfo> list_feed) {
-        this.list_feed = list_feed;
+    public FeedAdapter(ArrayList<BoardInfo> arrayList_feed,Context mContext) {
+        this.arrayList_feed = arrayList_feed;
         this.mContext = mContext;
-        this.mFirebaseUser = mFirebaseUser;
-        this.mCallback = mCallback;
-        this.mBoardName = mBoardName;
-        glide = Glide.with(mContext);
+
+
+//
     }
 
     public interface OnItemClickListener {
@@ -68,7 +68,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder
 
     @Override
     public void onBindViewHolder(final FeedViewHolder holder, int position) {
-        final BoardInfo modelFeed = list_feed.get(position);
+        final BoardInfo modelFeed = arrayList_feed.get(position);
         final String documentId = modelFeed.getDocumentId();
         holder.tv_boardtitle.setText(modelFeed.getBoardTitle());
         holder.tv_feedtitle.setText(modelFeed.getTitle());
@@ -111,6 +111,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder
                 }
             }
         });
+       // glide = Glide.with(mContext);
         /* 이미지그려주기
         if (modelFeed.getmDownloadURIList().size() != 0) {
             Glide.with(holder.img_post).load(modelFeed.getmDownloadURIList().get(0)).into(holder.img_post);
@@ -141,7 +142,7 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder
 
     @Override
     public int getItemCount() {
-        return list_feed.size();
+        return arrayList_feed.size();
     }
 
     class FeedViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
+++ app/src/main/java/com/kuple/zone/navigation/HomeFragment.java
@@ -9,11 +9,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,9 +24,12 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.kuple.zone.Adapter.ChildAdapter;
 import com.kuple.zone.Adapter.FeedAdapter;
 import com.kuple.zone.Adapter.HorizentalAdapter;
+import com.kuple.zone.Inteface.OnItemClick;
 import com.kuple.zone.R;
 import com.kuple.zone.board.CommonboardActivity;
 import com.kuple.zone.board.WebViewActivity;
@@ -35,18 +41,29 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private ArrayList<BoardInfo> modelFeedArrayList;
-    private RecyclerView.Adapter mAdapter;
+    private FeedAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+    //private OnItemClick mCallback;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.feedKuple);
-        mAdapter = new FeedAdapter(modelFeedArrayList);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(mAdapter);
+        String[] boardarray ={"쿠플광장","고민상담","쑥덕쑥덕","졸업생 게시판","쿠플툰","먹쿠먹쿠","강의평가","합격수기","취업광장","스터디게시판","꿀팁게시판"
+                ,"부동산","구인구직","중고거래","분실물신고","총학생회"} ;
+        for(String s:boardarray){
+            RetrieveFireStore(s);
+        }
+
+
+//        mAdapter = new FeedAdapter(modelFeedArrayList);
+//
+//
+//        mRecyclerView.setHasFixedSize(true);
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        mRecyclerView.setAdapter(mAdapter);
+
 
         Log.e("fragment", "HomeFragment");
         ImageView btnMeal = (ImageView) view.findViewById(R.id.btnMeal);
@@ -88,5 +105,26 @@ public class HomeFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void RetrieveFireStore(String collectionname) {
+
+        mStore.collection(collectionname).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                ArrayList<BoardInfo> feed_list=new ArrayList<>();
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                       BoardInfo boardInfo=document.toObject(BoardInfo.class);
+                        feed_list.add(boardInfo);
+                    }
+                    FeedAdapter feedAdapter=new FeedAdapter(feed_list,getContext());
+                    mRecyclerView.setAdapter(feedAdapter);
+
+                } else {
+
+                }
+            }
+        });
     }
 }
